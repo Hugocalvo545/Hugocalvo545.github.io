15fadd824a12b9b62e08605435028b0ef3550a3e
Hugocalvo545 <161371278+Hugocalvo545@users.noreply.github.com>
2025-04-02 20:58:22 +0200

    Update background.js

--- porfolio/js/background.js
+++ porfolio/js/background.js
@@ -22,7 +22,16 @@ document.addEventListener('DOMContentLoaded', function() {
                     maxParticles: 30,
                     connectParticles: false
                 }
-            }
+            },{
+             interactivity: {
+                 events: {
+                     onhover: {
+                         enable: true,
+                         mode: 'repulse'
+                     }
+                 }
+             }
+             }
         ]
     });
 });
