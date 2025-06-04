d0350a6f8118736e582c44909bfab5d096c59dce
Hugocalvo545 <161371278+Hugocalvo545@users.noreply.github.com>
2025-06-04 18:40:03 +0200

    Add files via upload

---
+++ porfolio/js/typed_Eng.js
@@ -0,0 +1,23 @@
+var typed = new Typed('#typed', {
+  strings: [
+    'I am detail-oriented and careful',
+    'I am creative',
+    'I am a good teammate',
+    'I am pleasant and attentive',
+    'I am a good advisor',
+    'I am eager to learn',
+    'I build relationships easily',
+    'I have good people skills',
+    'I learn quickly',
+    'I solve problems',
+    'I work very well in a team',
+  ],
+  typeSpeed: 50,
+  backSpeed: 25,
+  backDelay: 1500,
+  startDelay: 500,
+  loop: true,
+  showCursor: true,
+  cursorChar: '|',
+  smartBackspace: true
+});
